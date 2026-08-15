8bfdc9570103aa1e1c07748b86744eae04751ad4
pankaj yadav <pankajyadavrhg@gmail.com>
2026-08-15 14:40:39 +0530

    fix terraform formatting

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "Test-rg"                # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name = "pankajstorage"          # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name       = "statefile"              # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
+    resource_group_name  = "Test-rg"       # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
+    storage_account_name = "pankajstorage" # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
+    container_name       = "statefile"     # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
     key                  = "prod.terraform.tfstate"
-    use_oidc       = "true" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    use_oidc             = "true" # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
   required_providers {
     azurerm = {
